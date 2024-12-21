8add719e18086056856fc70bd5e3bf95c465b208
Prantor-Hawlader <prantor502@gmail.comm>
2024-12-21 17:19:19 +0600

    Box created

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
 function App() {
   return (
-    <div className="border border-green-500 p-4 m-10">
-      <h1 className="text-3xl font-bold underline text-center">Hello world!</h1>
+    <div className="flex justify-center items-center h-screen w-full">
+      <div className="flex border border-purple-300 p-10 bg-cyan-200">
+        <button className="px-3 py-2 text-white bg-green-500 rounded-md font-semibold text-xl">
+          Increment
+        </button>
+        <h1 className="text-3xl mx-10">0</h1>
+        <button className="px-3 py-2 text-white bg-red-500 rounded-md font-semibold ">
+          Decrement
+        </button>
+      </div>
     </div>
   );
 }
