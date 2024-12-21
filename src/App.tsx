eb697eada358ddd2ceca0975d3298fd7521db05f
Prantor-Hawlader <prantor502@gmail.comm>
2024-12-21 16:02:28 +0600

    Project Initialized

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="border border-green-500 p-4 m-10">
+      <h1 className="text-3xl font-bold underline text-center">Hello world!</h1>
+    </div>
+  );
+}
+
+export default App;
